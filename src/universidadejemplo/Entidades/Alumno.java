ccc18db199cef2dca71ce485615e20a5f87df2a2
White <134107976+Witehs@users.noreply.github.com>
2023-09-11 15:50:34 -0300

    solucion Ubicacion Error

--- src/universidadejemplo/Entidades/Alumno.java
+++ src/universidadejemplo/Entidades/Alumno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package universidadequipo7.entidades;
+package universidadejemplo.Entidades;
 
 import java.time.LocalDate;
 
